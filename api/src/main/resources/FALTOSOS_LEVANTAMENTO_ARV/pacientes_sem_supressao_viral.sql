d7a7c849d7ecab31673272d73e16e31f5e3404e6
emaposse <eusebiomaposse@gmail.com>
2022-03-23 20:09:24 +0200

    bug fix faltosos arv.

--- api/src/main/resources/FALTOSOS_LEVANTAMENTO_ARV/pacientes_sem_supressao_viral.sql
+++ api/src/main/resources/FALTOSOS_LEVANTAMENTO_ARV/pacientes_sem_supressao_viral.sql
@@ -43,14 +43,10 @@ from	(
 																		and e.location_id =:location and o.obs_datetime between date_sub(:endDate, interval 12 month) and :endDate
 																	group by p.patient_id
 
-															  ) 
-														cargaQuantitaviva group by patient_id, encounter_datetime desc, ordem asc
-						   							) 
-						   						cargaQuantitaviva group by patient_id
-											) 
-										cargaQuantitativa group by patient_id
-									) 
-								cargaQuantitativa
+															  ) cargaQuantitaviva group by patient_id, encounter_datetime desc, ordem asc
+						   							) cargaQuantitaviva group by patient_id
+											) cargaQuantitativa group by patient_id
+									) cargaQuantitativa
 								left join 
 									(
 								 		select patient_id, encounter_datetime, cargaQualitativa.ordem
@@ -78,26 +74,25 @@ from	(
 									              		inner join obs o on o.encounter_id = e.encounter_id
 									           	where p.voided = 0  and e.voided = 0 and o.voided = 0 and e.encounter_type  = 53 and o.concept_id = 1305 
 									               	and e.location_id =:location and o.obs_datetime between date_sub(:endDate, interval 12 month) and :endDate
-											) 
-										cargaQualitativa
-									)  
-								cargaQualitativa on cargaQuantitativa.patient_id = cargaQualitativa.patient_id 
-			       				where cargaQualitativa.patient_id is null  
+											) cargaQualitativa
+								 	)  cargaQualitativa on cargaQuantitativa.patient_id = cargaQualitativa.patient_id 
+			       				     where cargaQualitativa.patient_id is null  
 			       					or ( cargaQuantitativa.encounter_datetime > cargaQualitativa.encounter_datetime) 
 			       					or ( cargaQuantitativa.encounter_datetime = cargaQualitativa.encounter_datetime and cargaQuantitativa.ordem <= cargaQualitativa.ordem )  
-							) 
-						cargaQuantitativa
+							) cargaQuantitativa
 							join 
 					        (
 					            select p.patient_id, max(e.encounter_datetime) encounter_datetime from  patient p
 								inner join encounter e on e.patient_id = p.patient_id 
 								inner join obs o on o.encounter_id = e.encounter_id
-								where e.voided = 0 and o.voided =0 and e.encounter_type in (13, 51, 6) and o.concept_id = 856 and e.location_id =:location 
+								where e.voided = 0 and o.voided =0 and e.encounter_type in (13, 51, 6,53) and o.concept_id = 856 and e.location_id =:location 
 								and e.encounter_datetime between date_sub(:endDate, interval 12 month) and :endDate and o.value_numeric >= 1000
 					            group by p.patient_id
 					
 						     ) filter on filter.patient_id=cargaQuantitativa.patient_id and filter.encounter_datetime=cargaQuantitativa.encounter_datetime
-						union
+						
+					    union
+						
 						select cargaQuantitativa.patient_id, cargaQuantitativa.obs_datetime encounter_datetime , 2 as ordem
 						from (
 								select cargaQuantitativa.patient_id, cargaQuantitativa.obs_datetime
@@ -126,21 +121,17 @@ from	(
 						       		)   
 						       	cargaQualitativa on cargaQuantitativa.patient_id = cargaQualitativa.patient_id 
 						   		where cargaQualitativa.patient_id is null  or cargaQuantitativa.obs_datetime >= cargaQualitativa.obs_datetime
-							) 
-						cargaQuantitativa
+							) cargaQuantitativa
 							join 
 					        (
 					            select p.patient_id, max(e.encounter_datetime) encounter_datetime from  patient p
 								inner join encounter e on e.patient_id = p.patient_id 
 								inner join obs o on o.encounter_id = e.encounter_id
-								where e.voided = 0 and o.voided =0 and e.encounter_type in (13, 51, 6) and o.concept_id = 856 and e.location_id =:location 
+								where e.voided = 0 and o.voided =0 and e.encounter_type in (13, 51, 6,53) and o.concept_id = 856 and e.location_id =:location 
 								and e.encounter_datetime between date_sub(:endDate, interval 12 month) and :endDate and o.value_numeric >= 1000
 					            group by p.patient_id
 					
 						     ) filter on filter.patient_id=cargaQuantitativa.patient_id and filter.encounter_datetime=cargaQuantitativa.obs_datetime
-
-				     ) 
-				cargaQuantitativa group by patient_id, encounter_datetime desc, ordem asc 
-			) 
-		cargaQuantitativa group by patient_id
+				     ) cargaQuantitativa group by patient_id, encounter_datetime desc, ordem asc 
+			) cargaQuantitativa group by patient_id
 	) cargaQuantitativa
