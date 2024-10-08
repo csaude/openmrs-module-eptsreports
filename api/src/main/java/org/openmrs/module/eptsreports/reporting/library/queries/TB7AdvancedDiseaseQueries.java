4db60293a40bba70564cba7a3d8563cc9efdb911
nelsonhomo <nelson.homo@csaude.org.mz>
2024-10-08 13:19:13 +0200

    SESP-313: Fix tblam results query for cascade 1 Indicator

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/TB7AdvancedDiseaseQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/TB7AdvancedDiseaseQueries.java
@@ -280,7 +280,7 @@ public interface TB7AdvancedDiseaseQueries {
             + "  inner join encounter e on e.patient_id=p.patient_id                                                                                         "
             + "  inner join obs o on o.encounter_id=e.encounter_id                                                                                           "
             + "where p.voided=0 and  e.voided=0 and e.encounter_type in (6, 13, 51) and o.concept_id=23951 and o.value_coded is not null  and o.voided=0 "
-            + "  and e.location_id=:location and e.encounter_datetime between :startDate and :endDate                                                  "
+            + "  and e.location_id=:location and e.encounter_datetime between :endDate and CURDATE()                                                  "
             + "union                                                                                                                                       "
             + "                                                                                                                                            "
             + "select p.patient_id                                                                                                                         "
