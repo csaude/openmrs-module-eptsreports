3f26063c759185a7d124ca94d36b57ca262741ef
emaposse <eusebiomaposse@gmail.com>
2024-11-28 17:15:21 +0200

    SESP-287:Bug Fix Ficha Recepcao Levantou Period.

--- api/src/main/resources/LIST_PATIENTS_ELEGIBLE_VIRAL_LOAD_SAMPLE_COLLECTION/PATIENTS_ELEGIBLE_VIRAL_LOAD_SAMPLE_COLLECTION_TOTAL.sql
+++ api/src/main/resources/LIST_PATIENTS_ELEGIBLE_VIRAL_LOAD_SAMPLE_COLLECTION/PATIENTS_ELEGIBLE_VIRAL_LOAD_SAMPLE_COLLECTION_TOTAL.sql
@@ -38,7 +38,7 @@
             					inner join obs o on e.encounter_id=o.encounter_id 
             			where 	p.voided=0 and e.voided=0 and o.voided=0 and e.encounter_type=52 and 
             					o.concept_id=23866 and o.value_datetime is not null and 
-            					o.value_datetime<= CURDATE() and e.location_id=:location
+            					o.value_datetime<= :endDate and e.location_id=:location
             					group by p.patient_id
             					) maxFicha where (data_levantamento + INTERVAL 30 day) between  :startDate and :endDate 
             					group by maxFicha.patient_id
@@ -131,7 +131,7 @@
             					inner join obs o on e.encounter_id=o.encounter_id 
             			where 	p.voided=0 and e.voided=0 and o.voided=0 and e.encounter_type=52 and 
             					o.concept_id=23866 and o.value_datetime is not null and 
-            					o.value_datetime<= :startDate and e.location_id=:location 
+            					o.value_datetime<= :endDate and e.location_id=:location 
             					order by p.patient_id, value_datetime desc
             					) maxFicha where (data_levantamento + INTERVAL 30 day) between  :startDate and :endDate 
             					group by maxFicha.patient_id
