2e31f4a8b2f41ff4fd6204bac21dc382ce0387b3
emaposse <eusebiomaposse@gmail.com>
2025-03-10 11:16:11 +0200

    SESP-399:Bug Fix On Resumo Mensal Prep.

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/PrepKeyPopQuery.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/PrepKeyPopQuery.java
@@ -279,7 +279,7 @@ public class PrepKeyPopQuery {
         break;
 
       case OTHER:
-        query = query + "where value_coded=5622 ";
+        query = query + "where value_coded=5622 or value_coded=165205 ";
         break;
     }
 
