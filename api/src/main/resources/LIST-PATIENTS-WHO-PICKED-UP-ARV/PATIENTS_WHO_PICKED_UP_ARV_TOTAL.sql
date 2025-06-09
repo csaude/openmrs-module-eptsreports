2f4f3bfc8c94e908c665dd56d911badc1efde7a2
nelsonhomo <nelson.homo@csaude.org.mz>
2025-06-09 14:14:00 +0200

    SESP-408: Fix formulations query

--- api/src/main/resources/LIST-PATIENTS-WHO-PICKED-UP-ARV/PATIENTS_WHO_PICKED_UP_ARV_TOTAL.sql
+++ api/src/main/resources/LIST-PATIENTS-WHO-PICKED-UP-ARV/PATIENTS_WHO_PICKED_UP_ARV_TOTAL.sql
@@ -334,6 +334,7 @@
 			select * from (
 			SELECT e.patient_id, e.encounter_id FROM encounter e 
 			where e.encounter_type = 18 and e.encounter_datetime <= :endDate
+			and e.voided = 0 and e.location_id=:location
 			order by e.patient_id, e.encounter_id desc
 			)maxConsulta group by maxConsulta.patient_id
 			)maxConsulta on maxConsulta.patient_id = formulacoes.person_id and formulacoes.encounter_id = maxConsulta.encounter_id
