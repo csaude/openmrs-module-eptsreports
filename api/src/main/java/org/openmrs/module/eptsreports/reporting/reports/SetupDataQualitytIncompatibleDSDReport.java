420b55ce81ea833a6739755bfd4587cf6e37bdf4
nelsonhomo <nelson.homo@csaude.org.mz>
2025-11-11 10:00:49 +0200

    SESP-460: RQD4 Include PID Disclamer in list template

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupDataQualitytIncompatibleDSDReport.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupDataQualitytIncompatibleDSDReport.java
@@ -124,7 +124,7 @@ public class SetupDataQualitytIncompatibleDSDReport extends EptsDataExportManage
               getExcelDesignUuid(),
               null);
       Properties props = new Properties();
-      props.put("repeatingSections", "sheet:2,row:7,dataset:EC1 | sheet:3,row:7,dataset:EC2");
+      props.put("repeatingSections", "sheet:2,row:8,dataset:EC1 | sheet:3,row:8,dataset:EC2");
       props.put("sortWeight", "5000");
       props.put("sortWeight", "5000");
       reportDesign.setProperties(props);
